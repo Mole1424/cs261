daa555e4f3c885c00ef562ccc425870c1610f16f
triflicacid <rubensaunders@hotmail.co.uk>
2024-02-06 12:10:35 +0000

    .env file is now loaded

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from server import constants
 from server.app import create_app
+from dotenv import load_dotenv
+
 
 if __name__ == "__main__":
+    # Load .env file into environment
+    load_dotenv()
+
+    # Create and launch Flask application
     app = create_app()
     app.run(
         host=constants.FLASK_HOST,
