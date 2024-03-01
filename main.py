65280874c305acb23ae0fbcdb41f88bc09f3f1ca
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2024-03-01 15:40:43 +0000

    now properly multithreaded

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sys import argv
 from server import constants
 from server.app import create_app
 from dotenv import load_dotenv
-import threading
+from multiprocessing import Process
 from data.interface import update_loop
 
 if __name__ == "__main__":
@@ -15,8 +15,9 @@ if __name__ == "__main__":
     app = create_app()
 
     # Start the update loop DONT UNCOMMENT THIS AS ITS UNTESTED
-    # update_thread = threading.Thread(target=update_loop, args=(app,))
-    # update_thread.start()
+    # p = Process(target=update_loop, args=(app,))
+    # p.start()
+    # p.join()
 
     app.run(
         host=constants.FLASK_HOST, port=constants.FLASK_PORT, debug="--debug" in options
